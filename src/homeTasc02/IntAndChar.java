5a8bf9299cf49ea27bd6f00e452bc092aedeaf4b
Nastya <aa_lopata@mail.ru>
2015-02-12 17:55:35 +0200

    The program dose not work with out of initialization variables.

---
+++ src/homeTasc02/IntAndChar.java
@@ -0,0 +1,12 @@
+package homeTasc02;
+
+public class IntAndChar {
+    public static void main(String[] args) {
+        int i;
+        char p;
+        System.out.println(i);
+        System.out.println(p);
+    }
+
+
+}
