54e6e5521089c9281fb167ddc40ae3ad8ec11327
Nastya <aa_lopata@mail.ru>
2015-02-12 20:06:55 +0200

    Check for nil.

--- src/homeTasc02/QuadraticEquation.java
+++ src/homeTasc02/QuadraticEquation.java
@@ -13,26 +13,46 @@ public class QuadraticEquation {
         double x1;
         double x2;
         double D;
+        boolean chek = false;
 
         System.out.println("Решение уравнения вида ax^2+bx+c=0");
-        Scanner scn= new Scanner(System.in);
+        Scanner scn = new Scanner(System.in);
 
         System.out.println("Введите значение а:");
-        a= scn.nextDouble();
-        if(a==0){
+        a = scn.nextDouble();
+        if (a == 0) {
             System.out.println("Ноль использовать нельзя");
+        } else {
+            System.out.println("Введите значение b:");
+            b = scn.nextDouble();
+            if (b == 0) {
+                System.out.println("Ноль использовать нельзя");
+            } else {
+                System.out.println("Введите значение c:");
+                c = scn.nextDouble();
+                if (c == 0) {
+                    System.out.println("Ноль использовать нельзя");
+                }else {
+
+                D = Math.sqrt((b * b) - 4 * a * c);
+                if (D < 0) {
+                    System.out.println("Дуйствительных корней нет");
+                } else if (D == 0) {
+                    x1 = ((-b + D) / 2 * a);
+                    x2 = x1;
+                    System.out.println("x1 = x2" + x1);
+
+                } else {
+                    System.out.println("D = " + D);
+
+                    x1 = ((-b + D) / 2 * a);
+                    System.out.println("x1 = " + x1);
+                    x2 = ((-b - D) / 2 * a);
+                    System.out.println("x1 = " + x1);
+                }
+            }
+
         }
-        System.out.println("Введите значение b:");
-        b= scn.nextDouble();
-        System.out.println("Введите значение c:");
-        c= scn.nextDouble();
+    }}}
 
 
-        D=Math.sqrt((b*b)-4*a*c);
-        System.out.println("D = "+D);
-        x1=((-b+D)/2*a);
-        System.out.println("x1 = "+x1);
-
-
-    }
-}
