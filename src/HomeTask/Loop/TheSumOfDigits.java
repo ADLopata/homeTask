5e4bbddec0fd5cff618d6f2f20c672efeed43b49
Nastya <aa_lopata@mail.ru>
2015-02-15 18:47:05 +0200

    sum of the numbers

--- src/HomeTask/Loop/TheSumOfDigits.java
+++
@@ -1,7 +0,0 @@
-package HomeTask.Loop;
-
-/**
- * Created by Персикс on 15.02.2015.
- */
-public class TheSumOfDigits {
-}
---
+++ src/homeTask03/Loop/SumOfNunbers.java
@@ -0,0 +1,23 @@
+package homeTask03.Loop;
+
+
+import java.util.Scanner;
+
+public class SumOfNunbers {
+
+    public static void main(String[] args) {
+        int a,c=0;
+        System.out.println("enter the number");
+        Scanner scn=new Scanner(System.in);
+        if(scn.hasNextInt()){
+            a=scn.nextInt();
+            while(a!=0){
+                c=c+a%10;
+                a/=10;
+            }
+
+            System.out.println("The sum of all numbers is equal to the number entered "+c);
+        }
+        else System.out.println("Error. You entered is not a number!");
+    }
+}
