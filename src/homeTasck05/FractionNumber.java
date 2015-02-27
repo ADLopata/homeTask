14fd70bd5b06735e9d55fa8d73a3703de4bd4aa5
Nastya <aa_lopata@mail.ru>
2015-02-28 00:20:57 +0200

    done

---
+++ src/homeTasck05/FractionNumber.java
@@ -0,0 +1,53 @@
+package homeTasck05;
+
+/**
+ * Created by Персикс on 27.02.2015.
+ */
+public class FractionNumber implements com.sourceit.hometask.basic.FractionNumber {
+
+    private int dividend;//делимое
+    private int divisor;//делитель
+    private static final int	DEFAULT_DIVISOR_VALUE =1 ;
+
+
+    @Override
+    public void setDividend(int dividend) {
+     this.dividend= dividend;
+    }
+
+    @Override
+    public int getDividend() {
+        return dividend;
+    }
+
+    @Override
+    public void setDivisor(int divisor) {
+    this.divisor= divisor;
+    }
+
+    @Override
+    public int getDivisor() {
+        return divisor;
+    }
+
+    @Override
+    public double doubleValue() {
+        return ((double)dividend/divisor);
+    }
+
+    @Override
+    public int compareTo(com.sourceit.hometask.basic.FractionNumber obj) {
+        if(doubleValue()<obj.doubleValue()) {
+            return -1;
+        }
+         else if (doubleValue()>obj.doubleValue()){
+            return 1;
+        }
+           return 0;
+    }
+
+   public String toString(){
+       return getDividend()+"/"+getDivisor();
+   }
+
+}
---
+++ src/homeTasck05/FractionNumberOperation.java
@@ -0,0 +1,38 @@
+package homeTasck05;
+
+import com.sourceit.hometask.basic.*;
+import com.sourceit.hometask.basic.FractionNumber;
+
+/**
+ * Created by Персикс on 27.02.2015.
+ */
+public class FractionNumberOperation implements com.sourceit.hometask.basic.FractionNumberOperation {
+    @Override
+    public FractionNumber add(FractionNumber firstNum, FractionNumber secondNum) {
+        FractionNumber fno= new FractionNumber();
+        fno.setDivisor( firstNum.getDivisor()*secondNum.getDivisor());
+        fno.setDividend(firstNum.getDividend()*secondNum.getDivisor()+secondNum.getDividend()*firstNum.getDivisor());
+
+        return fno;
+    }
+
+    @Override
+    public FractionNumber sub(FractionNumber fractionNumber, FractionNumber fractionNumber1) {
+        return null;
+    }
+
+    @Override
+    public FractionNumber mul(FractionNumber fractionNumber, FractionNumber fractionNumber1) {
+        return null;
+    }
+
+    @Override
+    public FractionNumber div(FractionNumber fractionNumber, FractionNumber fractionNumber1) {
+        return null;
+    }
+
+    @Override
+    public FractionNumber parseFractionNumber(String s) {
+        return null;
+    }
+}
