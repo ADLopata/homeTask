eadb4d08cdbd823dbe726afac2b51859a12b9515
Nastya <aa_lopata@mail.ru>
2015-03-10 12:07:00 +0200

    do task by an arrays.

--- src/homeWork06/StringUtilsImpl.java
+++ src/homeWork06/StringUtilsImpl.java
@@ -13,7 +13,7 @@ public class StringUtilsImpl implements StringUtils {
         String str = "null";
         if (str.equals(dividend)) throw new NullPointerException("NullPointerException: dividend is null");
         if (str.equals(divisor)) throw new NullPointerException("NullPointerException: divisor is null");
-
+            // проверяем строку на соответствие регулярному выражению \\D* не цифровой символ
         if (dividend.matches("\\D*")) throw new NumberFormatException("NumberFormatException: dividend is not number");
         if (divisor.matches("\\D*")) throw new NumberFormatException("NumberFormatException: divisor is not number");
 
@@ -29,36 +29,56 @@ public class StringUtilsImpl implements StringUtils {
         String str = " null";
         if (text.equals(str)) throw new NullPointerException("NullPointerException: text are equal null");
         if (word.equals(str)) throw new NullPointerException("NullPointerException: word are equal null");
-        // ищет совпадение в строке и разберает ее на массив
-        String[] srtArr = text.split("");
+        // ищет совпадение в строке и разберает ее на массив по пробелам
+        String[] srtArr = text.split(" ");
+        //создаем масив для подсчета индекса слова
+        int[] arrOfInd = {};
+        int calcInd = 0;
         //ищем индексы  разобранной строки
-        for (String x : srtArr) {
-            System.out.print(" " + x);
+       /* for (int p = 0; p < srtArr.length; p++) {
+            System.out.print("  " + p);
+            создаем споисочный массив поиска  индекса слов
+              ArrayList<Integer> fouWord = new ArrayList<Integer>();*/
+
+        for (int i = 0; i < srtArr.length; i++) {
+
+            if (srtArr[i].equals(word)) {
+                arrOfInd[i] = calcInd++;
+            } else if (srtArr.length == 0)
+                throw new NullPointerException("NullPointerException:is no occurrence of finding word");
+
         }
-        //создаем споисочный массив поиска  индекса слов
-        ArrayList fouWord = new ArrayList<Integer>();
+        return arrOfInd;
+    }
 
-        for (int i = 0; i < srtArr.length; ) {
-            if (srtArr[i].equals(fouWord)) {
-                fouWord.add(i);
-            }
-
-            }
+        /*// если длинна слова в массиве равна 0
             if (fouWord.toArray().length == 0)
                 throw new NullPointerException("NullPointerException: is no occurrence of finding word");
 
-
            int[] foundWordInt = new int[(fouWord.toArray()).length];
             for (int r = 0; r < foundWordInt.length; r++) {
-                foundWordInt[r] = (int)((fouWord.toArray())[r]);
+                foundWordInt[r] = (Integer)((fouWord.toArray())[r]);
             }
-        return foundWordInt;
+        return foundWordInt;*/
 
-    }
+
 
 
     @Override
-    public double[] findNumbers(String s) throws CustomException {
-        return new double[0];
+    public double[] findNumbers(String text) throws CustomException {
+        String []srtArrDoub= text.split(" ");
+        double[] arrOfDoub={};
+        double calcIndOfDoubl=0;
+        for (int i=0;i<srtArrDoub.length;i++){
+          //проверка строки текста является ли он вещественным числом
+            //случай, когда вещественное число может быть как в обычной форме ( 256.789 ),
+          // так и в экспоненциальной форме ( 2.56789e+02 или 2.56789E+02 ).
+            if(text.matches("\"[-+]?[0-9]*\\\\.?[0-9]+([eE][-+]?[0-9]+)?\"")){
+                arrOfDoub[i]=calcIndOfDoubl++;
+
+            }
+            else if(srtArrDoub.length==0) throw new CustomException("CustomException:no double value was found in the text ");
+        }
+        return arrOfDoub;
     }
 }
