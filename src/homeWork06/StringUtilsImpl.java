e0188b7be275c2e8415f505d6e665a5207291974
Nastya <aa_lopata@mail.ru>
2015-03-12 01:20:57 +0200

    The Interface's methods  implementation without Collections

--- src/homeWork06/StringUtilsImpl.java
+++ src/homeWork06/StringUtilsImpl.java
@@ -2,6 +2,8 @@ package homeWork06;
 
 import com.sourceit.hometask.exceptions.CustomException;
 import com.sourceit.hometask.exceptions.StringUtils;
+import javafx.scene.shape.Path;
+
 import java.util.*;
 /**
  * Created by Персикс on 06.03.2015.
@@ -13,8 +15,9 @@ public class StringUtilsImpl implements StringUtils {
         String str = "null";
         if (str.equals(dividend)) throw new NullPointerException("NullPointerException: dividend is null");
         if (str.equals(divisor)) throw new NullPointerException("NullPointerException: divisor is null");
-            // проверяем строку на соответствие регулярному выражению \\D* не цифровой символ
-        if (dividend.matches("\\D*")) throw new NumberFormatException("NumberFormatException: dividend is not number");
+        // проверяем строку на соответствие регулярному выражению \\D* не цифровой символ
+        if (dividend.matches("\\D*")) throw new NumberFormatException("NumberFormatException:" +
+                "dividend is not number");
         if (divisor.matches("\\D*")) throw new NumberFormatException("NumberFormatException: divisor is not number");
 
         double divid = (Double.parseDouble(dividend));
@@ -26,59 +29,54 @@ public class StringUtilsImpl implements StringUtils {
 
     @Override
     public int[] findWord(String text, String word) throws NullPointerException {
-        String str = " null";
-        if (text.equals(str)) throw new NullPointerException("NullPointerException: text are equal null");
-        if (word.equals(str)) throw new NullPointerException("NullPointerException: word are equal null");
+        if (text == null) throw new NullPointerException("NullPointerException: text are equal null");
+        if (word == null) throw new NullPointerException("NullPointerException: word are equal null");
         // ищет совпадение в строке и разберает ее на массив по пробелам
-        String[] srtArr = text.split(" ");
+        String[] textArr = text.split(" ");
         //создаем масив для подсчета индекса слова
-        int[] arrOfInd = {};
         int calcInd = 0;
         //ищем индексы  разобранной строки
-       /* for (int p = 0; p < srtArr.length; p++) {
-            System.out.print("  " + p);
-            создаем споисочный массив поиска  индекса слов
-              ArrayList<Integer> fouWord = new ArrayList<Integer>();*/
+        int findWords[] = new int[20];
 
-        for (int i = 0; i < srtArr.length; i++) {
+        for (int index = 0; index < textArr.length; index++) {
+            if (word.equals(textArr[index])) {
 
-            if (srtArr[i].equals(word)) {
-                arrOfInd[i] = calcInd++;
-            } else if (srtArr.length == 0)
-                throw new NullPointerException("NullPointerException:is no occurrence of finding word");
-
-        }
-        return arrOfInd;
-    }
-
-        /*// если длинна слова в массиве равна 0
-            if (fouWord.toArray().length == 0)
-                throw new NullPointerException("NullPointerException: is no occurrence of finding word");
-
-           int[] foundWordInt = new int[(fouWord.toArray()).length];
-            for (int r = 0; r < foundWordInt.length; r++) {
-                foundWordInt[r] = (Integer)((fouWord.toArray())[r]);
+                findWords[calcInd] = index;
+                calcInd++;
+                // System.out.println(calcInd);
             }
-        return foundWordInt;*/
 
-
-
+        }//создаем массив для того чтобы выводить индекс без нулевых значений
+        int[] in = new int[calcInd];
+        System.arraycopy(findWords, 0, in, 0, calcInd);
+        if (findWords.length == 0)
+            throw new NullPointerException("NullPointerException:is no occurrence of finding word");
+//
+        return in;
+    }
 
     @Override
     public double[] findNumbers(String text) throws CustomException {
-        String []srtArrDoub= text.split(" ");
-        double[] arrOfDoub={};
-        double calcIndOfDoubl=0;
-        for (int i=0;i<srtArrDoub.length;i++){
-          //проверка строки текста является ли он вещественным числом
-            //случай, когда вещественное число может быть как в обычной форме ( 256.789 ),
-          // так и в экспоненциальной форме ( 2.56789e+02 или 2.56789E+02 ).
-            if(text.matches("\"[-+]?[0-9]*\\\\.?[0-9]+([eE][-+]?[0-9]+)?\"")){
-                arrOfDoub[i]=calcIndOfDoubl++;
+        String[] srtArrDoub = text.split(" ");
+        double[] arrOfDoub = new double[10];
+        int calcIndOfDoubl = 0;
+        for (int i = 0; i < srtArrDoub.length; i++) {
+            //проверка строки текста является ли он вещественным числом
+
+               if(srtArrDoub[i].contains(".") ){
+
+                arrOfDoub[calcIndOfDoubl++] = Double.parseDouble(srtArrDoub[i]);
 
             }
-            else if(srtArrDoub.length==0) throw new CustomException("CustomException:no double value was found in the text ");
         }
-        return arrOfDoub;
+            double[]resArr= new double[calcIndOfDoubl];
+            System.arraycopy(arrOfDoub,0,resArr,0,calcIndOfDoubl);
+
+            if(srtArrDoub.length==0)throw new CustomException( "CustomException:"+
+                    "Not double value was found in the text");
+
+
+        return resArr;
     }
+
 }
